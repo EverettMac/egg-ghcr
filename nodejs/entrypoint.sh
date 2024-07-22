1d1fd283670cc0c439a9a7a815afec0c8c733d21
Lezetho <126505858+lezetho@users.noreply.github.com>
2024-07-22 14:40:04 -0400

    Update entrypoint.sh

--- nodejs/entrypoint.sh
+++ nodejs/entrypoint.sh
@@ -41,6 +41,10 @@ node -v
 # variable format of "${VARIABLE}" before evaluating the string and automatically
 # replacing the values.
 PARSED=$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g' | eval echo "$(cat -)")
+
+# Debugging output for PARSED command
+echo "Debug: Parsed command: $PARSED"
+
 # Display the command we're running in the output, and then execute it with the env
 # from the container itself.
 printf "\033[1m\033[33mcontainer@pterodactyl~ \033[0m%s\n" "$PARSED"
